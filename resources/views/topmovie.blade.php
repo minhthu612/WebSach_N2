9a81248860a8bff3f324f4e26be867f3a83c0efe
minhthu612 <hmt06122005@gmail.com>
2026-03-17 21:00:35 +0700

    7.7

---
+++ resources/views/topmovie.blade.php
@@ -0,0 +1,22 @@
+<center><h2>Danh sách phim điểm cao</h2></center>
+
+<table border="1">
+<tr>
+    <th>Tên phim</th>
+    <th>Ngày phát hành</th>
+    <th>Mô tả</th>
+    <th>Ảnh</th>
+</tr>
+
+@foreach($movies as $m)
+<tr>
+    <td>{{ $m->movie_name }}</td>
+    <td>{{ $m->release_date }}</td>
+    <td>{{ $m->overview }}</td>
+    <td>
+        <img src="{{ $m->image_link }}" width="100">
+    </td>
+</tr>
+@endforeach
+
+</table>
