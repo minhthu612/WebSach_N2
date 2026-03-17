cfaf2501da7bf519206af49169a104cd930e3ad1
minhthu612 <hmt06122005@gmail.com>
2026-03-17 18:49:41 +0700

    demo

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,32 +4,33 @@ namespace App\Http\Controllers;
 
 class Controller
 {
-
     function tt()
     {
         return "Trần Thị Thanh Tiền";
     }
+
     function ZanThi()
     {
         return "Đào Đặng Thuỳ Vân";
     }
+
     function HMT()
     {
         return "Huỳnh Minh Thư";
     }
+
     function VanCongThien()
     {
         return "Văn Công Thiện";
     }
+
     function tlda()
     {
         return "Trần Lê Đông Anh";
-
     }
+
     function hnt()
     {
         return "Hồ Ngọc Tiền";
-
     }
-
 }
