9c17cacad6b94ba91dcf25156aaa29c342181eed
minhthu612 <hmt06122005@gmail.com>
2026-03-18 10:08:09 +0700

    7.2

--- app/Http/Controllers/Movie_Controller.php
+++ app/Http/Controllers/Movie_Controller.php
@@ -66,14 +66,14 @@ class Movie_Controller extends Controller
 
     function binhchoncao()
     { 
-    $phim = DB::table("movie as p")
-    ->select("p.movie_name", "p.release_date", "p.vote_average")
-    ->orderBy("p.vote_average", "desc")
-    ->limit(10)
-    ->get();
-    
-     return view("Binh_chon_cao",compact("phim"));        
-     }
+        $phim = DB::table("movie as p")
+        ->select("p.movie_name", "p.release_date", "p.vote_average")
+        ->orderBy("p.vote_average", "desc")
+        ->limit(10)
+        ->get();
+        
+        return view("Binh_chon_cao",compact("phim"));        
+    }
 }
 
 
--- resources/views/Binh_chon_cao.blade.php
+++ resources/views/Binh_chon_cao.blade.php
@@ -1,20 +1,5 @@
-<?php
+<h2>Top 10 phim có điểm cao nhất</h2>
 
-namespace App\Http\Controllers;
-
-use Illuminate\Support\Facades\DB;
-
-class Movie_Controller extends Controller
-{
-
-  function binhchoncao()
-  {
-    $phim = DB::table("movie as p")
-    ->select("p.movie_name", "p.release_date", "p.vote_average")
-    ->orderBy("p.vote_average", "desc")
-    ->limit(10)
-    ->get();
-    
-     return view("Binh_chon_cao",compact("phim"));        
-  }
-}
+@foreach($phim as $p)
+    {{ $p->movie_name }} - {{ $p->release_date }} - {{ $p->vote_average }} <br>
+@endforeach
