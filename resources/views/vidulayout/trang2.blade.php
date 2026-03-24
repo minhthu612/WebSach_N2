bc88d2145e619766a8361254e2ca0c75d5e395b9
username <0303239230245@st.buh.edu.vn>
2026-03-24 10:40:43 +0700

    tt

---
+++ resources/views/vidulayout/trang2.blade.php
@@ -0,0 +1,13 @@
+<html>
+    @extends("layouts.main")
+    @section("title","Trang nội dung 2")
+    @section("content")
+    <div class="bg-primary m-5"
+
+    style='height:300px;display:flex;align-items:center; justify-content:center;color:white'>
+
+    NỘI DUNG CHÍNH CỦA TRANG 2
+    </div>
+
+    @endsection
+</html>
