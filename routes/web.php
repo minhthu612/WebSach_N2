7c7e8db5840f20e7ea5ba80e06e14264f0d206a5
minhthu612 <hmt06122005@gmail.com>
2026-03-30 19:15:16 +0700

    12

--- routes/web.php
+++ routes/web.php
@@ -1,70 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/formtinhtuoi', 'App\Http\Controllers\Age_Controller@formtinhtuoi');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('/tinhtuoi', 'App\Http\Controllers\Age_Controller@tinhtuoi');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get("/qlsach/theloai","App\Http\Controllers\Book_Controller@laythongtintheloai");
-Route::get("/qlsach/thongtinsach","App\Http\Controllers\Book_Controller@laythongtinsach");
-
-Route::get("/baitap2","App\Http\Controllers\Book_Controller1@thongtinsachkinhdien");
-
-Route::get("/themdulieu","App\Http\Controllers\Book_Controller2@themdulieu");
-Route::get("/xoadulieu","App\Http\Controllers\Book_Controller2@xoadulieu");
-Route::get("/suadulieu","App\Http\Controllers\Book_Controller2@suadulieu");
-Route::get("/qlsach/theloai","App\Http\Controllers\Book_Controller2@laythongtintheloai");
-Route::get("/qlsach/thongtinsach","App\Http\Controllers\Book_Controller2@laythongtinsach");
-
-Route::get("/tranthithanhtien","App\Http\Controllers\Controller@tt");
-
-
-Route::get("/DaoDangThuyVan","App\Http\Controllers\Controller@ZanThi");
-
-
-Route::get("/VanCongThien","App\Http\Controllers\Controller@VanCongThien");
-Route::get("/HuynhMinhThu","App\Http\Controllers\Controller@HMT");
-Route::get("/TranLeDongAnh","App\Http\Controllers\Controller@tlda");
-
-Route::get("/HoNgocTien","App\Http\Controllers\Controller@hnt");
-
-
-/*7.4*/
-Route::get("/topruntime","App\Http\Controllers\Movie_Controller@topRuntime");
-
-
-
-/*7.1*/
-Route::get("/genre","App\Http\Controllers\Movie_Controller@genre");
-
-/*7.7*/
-Route::get("/topmovie","App\Http\Controllers\Movie_Controller@topMovie");
-
-
-Route::get("/Hienthifilm","App\Http\Controllers\Movie_Controller@FilmCanada");
-
-
-Route::get("/binhchoncao","App\Http\Controllers\Movie_Controller@binhchoncao");
-
-Route::get("/top-budget","App\Http\Controllers\Movie_Controller@topBudget");
-
-Route::get("/C7_6","App\Http\Controllers\Movie_Controller@C7_6");
-
-
-
-/*TH1-VD1*/
-Route::get('/trang1','App\Http\Controllers\ViduLayoutController@trang1');
-Route::get('/trang2','App\Http\Controllers\ViduLayoutController@trang2');
-
-/*TH1-VD2*/
-Route::get('/sach','App\Http\Controllers\ViduLayoutController@sach');
-Route::get('/sach/theloai/{id}','App\Http\Controllers\ViduLayoutController@theloai');
-
-/*TH1-BT*/
-Route::get('sach/chitiet/{id}','App\Http\Controllers\ViduLayoutController@chitiet');
-
+require __DIR__.'/auth.php';
